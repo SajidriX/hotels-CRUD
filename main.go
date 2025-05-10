75689c33c0f7ca6e14666e3f03ccfcc4c487c534
sajison <sajidrix@gmail.com>
2025-05-10 11:54:37 +0300

    Welcome add

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"hotels/hotels"
 	"hotels/users"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
+
+func hi(c echo.Context) error {
+	return c.JSON(http.StatusOK, echo.Map{"message": "welcome!"})
+}
 
 func main() {
 
@@ -26,6 +31,7 @@ func main() {
 	e.DELETE("/hotelsDelete/:name", hotels.DeleteHotels)
 	e.GET("/hotelsByCoun/:country", hotels.GetHotelByCountry)
 	e.GET("hotelsByName/:name", hotels.GetHotelByName)
+	e.GET("/", hi)
 
 	e.Logger.Fatal(e.Start(":1228"))
 }
